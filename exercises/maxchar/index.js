e26572a5e2e505aabc63fa07d77c40bc64d7d6b0
Namhyuk Woo <alexnh97@gmail.com>
2021-12-09 21:39:29 -0800

    maxChar done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let chars = {};
+  for (const char of str) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  }
+  let highScore = 0;
+  let winner = "";
+  for (const c in chars) {
+    if (chars[c] > highScore) {
+      highScore = chars[c];
+      winner = `${c}`;
+    } else {
+      continue;
+    }
+  }
+  return winner;
+}
 
 module.exports = maxChar;
