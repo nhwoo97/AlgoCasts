bd245ab01ebb5bf5428f26cdb40a6de4d0bed154
Namhyuk Woo <alexnh97@gmail.com>
2021-12-12 21:04:31 -0800

    anagrams done

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,49 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  //Make stringA and stringB lower case and use replace() to replace the spaces
+  //Use a for loop to loop through stringA and use a hashmap to save the frequency of each letter
+  //Do the same thing for stringB
+  //Compare the result
+
+  let a = stringA.replace(/[^\w]/g, "");
+  let b = stringB.replace(/[^\w]/g, "");
+
+  if (a.length != b.length) {
+    return false;
+  }
+
+  hashA = {};
+  hashB = {};
+
+  for (const element of a) {
+    if (!hashA[element]) {
+      hashA[element] = 1;
+    } else {
+      hashA[element] = hashA[element] + 1;
+    }
+  }
+
+  for (const element of b) {
+    if (!hashB[element]) {
+      hashB[element] = 1;
+    } else {
+      hashB[element] = hashB[element] + 1;
+    }
+  }
+
+  for (const key in hashA) {
+    if (!hashB[key]) {
+      return false;
+    } else if (hashA[key] != hashB[key]) {
+      return false;
+    } else {
+      continue;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
